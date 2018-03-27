682db247c67f1fbb87190e4c8ed991c412f880b3
Dennis Lord <lorddennis1006@gmail.com>
2018-03-27 14:45:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ function updateObjectWithKeyAndValue(object, key, value){return Object.assign({}
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){(object[key]=value ); return object }
 function deleteFromObjectByKey(object, key){return delete object.key}
 function destructivelyDeleteFromObjectByKey(object, key){(delete object.key); return [object]}
-function destructivelyDeleteFromObjectByKey(object, key){delete object.key);return 
+function destructivelyDeleteFromObjectByKey(object, key){(delete object.key);return object}
