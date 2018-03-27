6c0513cd728c1c68d621c69275f5a4857049838c
Dennis Lord <lorddennis1006@gmail.com>
2018-03-27 14:53:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 var recipes = new Object ({})
 function updateObjectWithKeyAndValue(object, key, value){return Object.assign({}, object, {[key] : value})}
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){(object[key]=value ); return object }
-function deleteFromObjectByKey(object, key){return delete object.key}
+function deleteFromObjectByKey(object, key){var clone = Object.assign({}, object);delete clone[key]; return clone}
 function destructivelyDeleteFromObjectByKey(object, key){(delete object.key); return [object]}
 function destructivelyDeleteFromObjectByKey(object, key) {var newObject = Object.assign({},object);delete newObject[key];return newObject} 
-function deleteFromObjectByKey(object, key) {
-+  var clone = Object.assign({}, object);
-+  delete clone[key];
-+  return clone;
+ 
