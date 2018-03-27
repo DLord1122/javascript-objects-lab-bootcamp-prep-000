6c4bed58a7ad9eaa2e6d5819791a5022fa028ea7
Dennis Lord <lorddennis1006@gmail.com>
2018-03-27 14:26:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 var recipes = new Object ({})
-function updateObjectWithKeyAndValue9object, key, value){return Object.assign({})
+function updateObjectWithKeyAndValue9object, key, value){return Object.assign({} object,)
